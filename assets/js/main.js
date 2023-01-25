e993f290072f96d524d9b4b7d97299ff0d88ade8
= <=>
2023-01-25 14:54:48 +0100

    commit 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,8 +13,11 @@ let myTasks = {
     'TESTING': [],
     'COMPLETED': []
 }
-function dragTasks() {
+function dragTask() {
     const TASKS = doc.querySelectorAll('.tasks .task')
+    // this list sort tasks when hovering above task (when sorting elements in dom)
+    let newOrderedTasksList = null
+    let currentSection = null
     TASKS.forEach(task => {
         task.addEventListener('dragstart', function (e) { 
             // e.preventDefault()
@@ -22,26 +25,38 @@ function dragTasks() {
             // const data = {
             //     section: 
             // }
+            draggedElement = this
+            currentSection = draggedElement.parentElement.parentElement.id
             this.style.opacity = '.5'
             e.dataTransfer.setData('section', this.parentElement.parentElement.getAttribute('id'))
-            draggedElement = this
-            console.log('drag start : ', this)
+            // console.log('drag start : ', myTasks[currentSection])
             // e.dataTransfer.setData('draggedElement', JSON.stringify({elem: this}))
          })
          task.addEventListener('dragover', function(e) {
             if (draggedElement != this) {
+                const TASKS_CONTAINER = this.parentElement
+                const DRAGGED_ELEMENT_INDEX = myTasks[currentSection].findIndex(task => task.id == draggedElement.getAttribute('data-id'))
+                const HOVERED_ELEMENT_INDEX = myTasks[currentSection].findIndex(task => task.id == this.getAttribute('data-id'))
+                // console.log('HOVERED_ELEMENT_INDEX : ', HOVERED_ELEMENT_INDEX)
+                newOrderedTasksList = [...myTasks[currentSection]]
                 // height of element that we hover above it
                 const ELEMENT_CLIENT_HEIGHT = getComputedStyle(this).getPropertyValue('height').slice(0, -2)
                 // if we hover on the half top of element insert dragging element before it
+                console.log('dragin element position :::: ', DRAGGED_ELEMENT_INDEX)
                if (e.offsetY <= ELEMENT_CLIENT_HEIGHT / 2) {
-                // console.log(this.parentElement)
-                this.parentElement.insertBefore(draggedElement, this)
-                   console.log('greater than')
+                TASKS_CONTAINER.insertBefore(draggedElement, this)
+                // console.log('this id : ', this.getAttribute('data-id'))
+                changeElementPosition(newOrderedTasksList, DRAGGED_ELEMENT_INDEX, HOVERED_ELEMENT_INDEX)
+                    //  console.log('original list ===> ', myTasks[currentSection])
+
+                //    console.log('before ===> ', newOrderedTasksList)
                }
                 // if we hover on the half bottom of element insert dragging element after it
                else {
-                this.parentElement.insertBefore(draggedElement, this.nextElementSibling)
-                   console.log('less than')
+                TASKS_CONTAINER.insertBefore(draggedElement, this.nextElementSibling)
+                changeElementPosition(newOrderedTasksList, DRAGGED_ELEMENT_INDEX, HOVERED_ELEMENT_INDEX)
+                // console.log('original list ===> ', myTasks[currentSection])
+                // console.log('after ===> ', newOrderedTasksList)
                }
             }
             // console.log('drag over', this, e)
@@ -49,6 +64,21 @@ function dragTasks() {
          task.addEventListener("dragend", function () {
             // console.log('drag end', this)
             this.style.opacity = '1'
+            // newOrderedTasksList is null until we drag task above task
+            if (!!newOrderedTasksList) {
+                // check if the order of tasks changed or not if it change update list
+                if (JSON.stringify(newOrderedTasksList) != JSON.stringify(myTasks[currentSection])) {
+                    myTasks[currentSection] = [...newOrderedTasksList]
+                    console.log('list changed')
+                    console.log('newOrderedTasksList : ', newOrderedTasksList)
+                    console.log('myTasks[currentSection] : ', myTasks[currentSection])
+                    newOrderedTasksList = null
+                }
+                else {
+                    console.log('not changed')
+                    newOrderedTasksList = null
+                }
+            }
             draggedElement = null
           });
     })
@@ -88,31 +118,33 @@ TASKS_SECTION.forEach((section, i) => {
 // add todo task
 addTodoBtn.addEventListener('click', function() {
     addTask(TODO, this)
-    dragTasks() 
+    dragTask() 
     // console.log(tasks)
 })
 // add in progress task
 addInProgressBtn.addEventListener('click', function () {
     addTask(IN_PROGRESS, this)
-    dragTasks() 
+    dragTask() 
     console.log('progress')
 })
 
 // add testing task
 addTestingBtn.addEventListener('click', function () {
     addTask(TESTING, this)
-    dragTasks() 
+    dragTask() 
     console.log('testing')
 })
 
 // add completed task
 addCompletedBtn.addEventListener('click', function () {
     addTask(COMPLETED, this)
-    dragTasks() 
+    dragTask() 
     console.log('completed')
 })
 
-dragTasks()
+dragTask()
+let i = 1
+
 function taskCard(id) {
     return  `<div class="task" draggable="true" data-id="${id}">
     <div class="card-setting">
@@ -126,21 +158,21 @@ function taskCard(id) {
         <img src="./assets/images/icons8-empty-trash-30.png" alt="">
         <!-- <i class="fa-solid fa-trash"></i> -->
     </div>
-    <p contenteditable="true">New Todo Task ..</p>
+    <p contenteditable="true">New Todo Task .. ${i}</p>
 </div>`
 }
 
 
 // console.log(generateTaskId())
-
 function addTask(status, target) {
     const TASK = {
         id: generateTaskId(status),
-        content: 'New Todo Task',
+        content: 'New Todo Task ' + i,
         background: 'linear-gradient(to top, #accbee 0%, #e7f0fd 100%)',
     }
     myTasks[status].unshift(TASK)
     target.parentElement.nextElementSibling.innerHTML = taskCard(TASK.id) + target.parentElement.nextElementSibling.innerHTML
+    i++
 }
 
 // this function generate new task id
@@ -149,3 +181,15 @@ function generateTaskId() {
     return `${CURRENT_DATE.getDate()}${CURRENT_DATE.getMonth()}${CURRENT_DATE.getFullYear()}${CURRENT_DATE.getHours()}${CURRENT_DATE.getMinutes()}${CURRENT_DATE.getSeconds()}${Math.floor(Math.random() * (900 - 100)) + 100}`
 }
 
+// function change element position in list
+
+function changeElementPosition(arr, fromIndex, toIndex) {
+    const element = arr[fromIndex]
+    arr.splice(fromIndex, 1)
+    arr.splice(toIndex, 0, element)
+}
+const lool = [1, 2, 3, 4, 5]
+console.log('before change position: ', lool)
+
+changeElementPosition(lool, 4, 0)
+console.log('after change position: ', lool)
